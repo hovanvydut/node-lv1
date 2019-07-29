4878269447a12df46ec39ac3ebe99f3f28e4d0d8
vymio84 <vymio84@gmail.com>
2019-07-29 10:44:12 +0700

    Status Helper 2

--- routes/backend/items.js
+++ routes/backend/items.js
@@ -4,9 +4,31 @@ var router = express.Router();
 const ItemsModel = require("./../../schemas/items.js");
 const UtilsHelper = require("./../../helper/utils.js");
 
-router.get("/", (req, res) => {
-	let statusFilter = UtilsHelper.createFilterStatus();
-	ItemsModel.find({}).then(items => {
+// List items
+// router.get("/(:status)?", (req, res) => {
+// 	let statusFilter = UtilsHelper.createFilterStatus();
+
+// 	ItemsModel.find({}).then(items => {
+// 		res.render("pages/items/list", {
+// 			title: "Item List Page",
+// 			items,
+// 			statusFilter
+// 		});
+// 	});
+// });
+
+router.get("/(:status)?", (req, res) => {
+	let currentStatus = "all";
+	if (req.params.hasOwnProperty("status") & (req.params.status !== undefined)) {
+		currentStatus = req.params.status;
+	}
+	let statusFilter = UtilsHelper.createFilterStatus(currentStatus);
+	let condition = {};
+	if (currentStatus !== "all") {
+		condition = { status: currentStatus };
+	}
+
+	ItemsModel.find(condition).then(items => {
 		res.render("pages/items/list", {
 			title: "Item List Page",
 			items,
