0eade81cecf58c362053541f3191a5528ad6b5f8
vymio84 <vymio84@gmail.com>
2019-08-02 13:18:08 +0700

    page range

--- routes/backend/items.js
+++ routes/backend/items.js
@@ -4,10 +4,21 @@ var router = express.Router();
 const ItemsModel = require("./../../schemas/items.js");
 const UtilsHelper = require("./../../helper/utils.js");
 const ParamsHelper = require("./../../helper/params.js");
-
 router.get("/(:status)?", (req, res) => {
 	let currentStatus = ParamsHelper.getParam(req.params, "status", "all");
 	let keyword = ParamsHelper.getParam(req.query, "keyword", "");
+	let pagination = {
+		totalItems: 1,
+		totalItemsPerPage: 2,
+		currentPage: 1,
+		pageRanges: 3
+	};
+	pagination.currentPage = parseInt(
+		ParamsHelper.getParam(req.query, "page", 1)
+	);
+	let start = (pagination.currentPage - 1) * pagination.totalItemsPerPage;
+
+	// let end =(pagination.currentPage - 1) * pagination.totalItemsPerPage +pagination.totalItemsPerPage;
 
 	let statusFilter = UtilsHelper.createFilterStatus(currentStatus);
 	let condition = {};
@@ -24,15 +35,23 @@ router.get("/(:status)?", (req, res) => {
 		}
 	}
 
+	ItemsModel.countDocuments(condition).then(
+		data => (pagination.totalItems = data)
+	);
+
 	ItemsModel.find(condition)
 		.sort({ ordering: "ascending" })
+		.skip(start)
+		.limit(pagination.totalItemsPerPage)
 		.then(items => {
+			// items = items.slice(start, end); other way to config pagination instead for use skip and limit
 			res.render("pages/items/list", {
 				title: "Item List Page",
 				items,
 				statusFilter,
 				currentStatus,
-				keyword
+				keyword,
+				pagination
 			});
 		});
 });
